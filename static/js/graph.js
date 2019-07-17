1d6e83ac4b8d3092783e88509de50c8e7ada2353
Ubuntu <ubuntu@ip-172-31-82-129.ec2.internal>
2019-07-17 20:22:19 +0000

    Added 1st Graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,6 +3,28 @@ queue()
      .await(makeGraphs);
     
 function makeGraphs(error, ticketsData) {
-
+    var ndx = crossfilter(ticketsData);
     
+    show_case_owner(ndx);
+    
+    dc.renderAll();
+    
+}
+
+function show_case_owner(ndx) {
+    var dim = ndx.dimension(dc.pluck('CaseOwner'));
+    var group = dim.group();
+    
+    dc.barChart("#case-owner")
+        .width(650)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Case Owner")
+        .yAxis().ticks(20);
 }
