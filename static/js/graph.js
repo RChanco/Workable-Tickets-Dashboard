cc931b827a30df92d4ba5270bf2a858eeb89c06a
Ubuntu <ubuntu@ip-172-31-82-129.ec2.internal>
2019-07-18 14:11:21 +0000

    Created Case Country Graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,12 +5,22 @@ queue()
 function makeGraphs(error, ticketsData) {
     var ndx = crossfilter(ticketsData);
     
+    show_status_selector(ndx);
     show_case_owner(ndx);
+    show_case_country(ndx);
     
     dc.renderAll();
     
 }
 
+function show_status_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('Status'));
+    var group = dim.group();
+    
+    dc.selectMenu("#status-selector")
+        .dimension(dim)
+        .group(group);
+}
 function show_case_owner(ndx) {
     var dim = ndx.dimension(dc.pluck('CaseOwner'));
     var group = dim.group();
@@ -24,7 +34,23 @@ function show_case_owner(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
         .xAxisLabel("Case Owner")
+        .yAxis().ticks(20);
+}
+
+function show_case_country(ndx) {
+    var dim = ndx.dimension(dc.pluck('CaseCountry'));
+    var group = dim.group();
+    
+    dc.barChart("#case-country")
+        .width(650)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Case Country")
         .yAxis().ticks(20);
 }
