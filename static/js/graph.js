8b9ad5b8b43af729e2c4859de62ec9bf50d2a82e
Ubuntu <ubuntu@ip-172-31-82-129.ec2.internal>
2019-07-20 13:42:32 +0000

    Added the SLA Status Chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,9 +5,15 @@ queue()
 function makeGraphs(error, ticketsData) {
     var ndx = crossfilter(ticketsData);
     
+/*    DaysOldData.forEach(function(d){
+        d.DaysOld = parseInt(d.DaysOld);
+    })
+*/
     show_status_selector(ndx);
     show_case_owner(ndx);
+    show_sla_status(ndx);
     show_case_country(ndx);
+/*    show_case_days_old(ndx);  */
     
     dc.renderAll();
     
@@ -35,9 +41,74 @@ function show_case_owner(ndx) {
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Case Owner")
-        .yAxis().ticks(20); 
+        .yAxis().ticks(20) ; 
 }
 
+function show_sla_status(ndx) {
+	    var dim = ndx.dimension(dc.pluck('SLA status'));
+	    var group = dim.group();
+	    
+	    dc.barChart("#sla-status")
+	        .width(400)
+	        .height(300)
+	        .margins({top: 10, right: 50, bottom: 30, left: 50})
+	        .dimension(dim)
+	        .group(group)
+	        .transitionDuration(500)
+	        .x(d3.scale.ordinal())
+	        .xUnits(dc.units.ordinal)
+	        .xAxisLabel("SLA Status")
+	        .yAxis().ticks(20);
+	}
+
+
+/*
+function show_case_days_old(ndx) {
+    var dim = ndx.dimension(dc.pluck('CaseOwner'));
+    
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.DaysOld;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if(p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        } else {
+            p.total -= v.DaysOld;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+    
+    function initialise() {
+        return {count: 0, total: 0, average: 0};
+    }
+
+    var averageDaysOld = dim.group().reduce(add_item, remove_item, initialise); 
+    
+    dc.barChart("#average-days-old")
+	        .width(400)
+	        .height(300)
+	        .margins({top: 10, right: 50, bottom: 30, left: 50})
+	        .dimension(dim)
+	        .group(averageDaysOld)
+	        .valueAccessor(function(d){
+	            return d.value.average.toFixed(2);
+	        })
+	        .transitionDuration(500)
+	        .x(d3.scale.ordinal())
+	        .xUnits(dc.units.ordinal)
+	        .elasticY(true)
+	        .xAxisLabel("Gender")
+	        .yAxis().ticks(4);   
+
+}
+*/
 function show_case_country(ndx) {
     var dim = ndx.dimension(dc.pluck('CaseCountry'));
     var group = dim.group();
@@ -45,12 +116,52 @@ function show_case_country(ndx) {
     dc.pieChart("#case-country")
         .width(650)
         .height(300)
-/*        .margins({top: 10, right: 50, bottom: 30, left: 50})  */
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
-/*        .x(d3.scale.ordinal())
+}
+
+function show_days_old(ndx) {
+    var dim = ndx.dimension(dc.pluck('DaysOld'));
+    
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.DaysOld;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if(p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        } else {
+            p.total -= v.DaysOld;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+    
+    function initialise() {
+        return {count: 0, total: 0, average: 0};
+    }
+
+    var averageDaysOld = dim.group().reduce(add_item, remove_item, initialise);
+
+    dc.barChart("#average-salary")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(averageDaysOld)
+        .valueAccessor(function(d){
+            return d.value.average.toFixed(2);
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .xAxisLabel("Case Country")
-        .yAxis().ticks(20); */
+        .elasticY(true)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(4);
 }
